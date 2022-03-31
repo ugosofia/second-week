6c5b6a5ea76d1b6b25ac34481b1bea9f46ced7f1
UGO SOFIA <ugo.sofia97@gmail.com>
2022-03-31 17:03:25 +0200

    JPA utils

---
+++ src/main/java/com/corso/java/secondweek/utils/JPAUtils.java
@@ -0,0 +1,25 @@
+package com.corso.java.secondweek.utils;
+
+
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class JPAUtils {
+    private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
+    private static EntityManagerFactory factory;
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (factory == null) {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        return factory;
+    }
+
+    public static void shutdown() {
+        if (factory != null) {
+            factory.close();
+        }
+    }
+}
